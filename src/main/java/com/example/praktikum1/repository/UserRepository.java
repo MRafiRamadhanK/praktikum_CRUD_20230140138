ab996004c7d15aae757faf853c9c921ba2dd7956
rafirmdhn <raficool271003@gmail.com>
2026-03-02 15:31:22 +0700

    controller,mapper,repo,service dan lain lainnya

--- src/main/java/com/example/praktikum1/repository/UserRepository.java
+++ src/main/java/com/example/praktikum1/repository/UserRepository.java
@@ -4,5 +4,6 @@ import com.example.praktikum1.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-public class UserRepository extends JpaRepository<User, String> {
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
 }
